f8dc61380d59bcb049f9024d9af757cd36b41980
David Kowis <david_kowis@homedepot.com>
2016-06-22 16:59:56 -0500

    Cleaning out unuseful hubot stuff

--- app/controllers/HomeController.scala
+++ app/controllers/HomeController.scala
@@ -1,7 +1,7 @@
 package controllers
 
 import javax.inject._
-import play.api._
+
 import play.api.mvc._
 
 /**
@@ -18,7 +18,7 @@ class HomeController @Inject() extends Controller {
    * a path of `/`.
    */
   def index = Action {
-    Ok(views.html.index("Your new application is ready."))
+    Ok(views.html.index("Tracker App!"))
   }
 
 }
--- app/controllers/TrackerController.scala
+++ app/controllers/TrackerController.scala
@@ -2,14 +2,12 @@ package controllers
 
 import javax.inject._
 
-import actors.SlackBotActor
-import akka.actor.{ActorRef, ActorSystem}
+import akka.actor.ActorRef
 import play.api.Configuration
-import play.api.libs.json.{JsError, JsSuccess, Json}
 import play.api.mvc._
-import services.{IncomingMessage, Pivotal, SlackMessage}
+import services.Pivotal
 
-import scala.concurrent.{ExecutionContext, Future}
+import scala.concurrent.ExecutionContext
 
 /**
   * This controller creates an `Action` to handle HTTP requests to the
@@ -23,43 +21,5 @@ class TrackerController @Inject()(
                                    pivotal: Pivotal)(implicit context: ExecutionContext) extends Controller {
 
   //By injecting it, the actor should be ready to go
-
-  /**
-    * This needs to be an asynchronous action to go ask the tracker API for something
-    *
-    * @return
-    */
-  def itemDetails = Action.async(parse.json) { request =>
-    import services.HubotRequestImplicits._
-
-    request.body.validate[IncomingMessage] match {
-      case s: JsSuccess[IncomingMessage] => {
-        //call pivotal
-        //return that response!
-        val storyId = s.get.matches.head.toLong
-        //TODO: assuming my config exists, shame on me
-        val result = pivotal.storyDetails(config.getLong("project_id").get, storyId)
-        val what: Future[Result] = result.map { futureResult =>
-          //This *should* work
-          futureResult.map { actualResult =>
-            import services.SlackJsonImplicits._
-
-            //Wrap the actual result in a slack message
-            val slackMessage = SlackMessage(
-              attachments = List(actualResult),
-              channel = s.get.channel
-            )
-            Ok(Json.toJson(slackMessage))
-          }
-        } getOrElse {
-          Future(NotFound("Not found"))
-        }
-        what
-      }
-      case e: JsError => {
-        Future(BadRequest(JsError.toJson(e)))
-      }
-    }
-  }
-
+  //I don't think I need any actual items in here!
 }
